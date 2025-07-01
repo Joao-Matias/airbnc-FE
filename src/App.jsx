730155b0f76754afa065d0025a3a50f737ee5ba7
Joao Matias <jdmatias@live.com>
2025-07-01 13:20:21 +0100

    Home component started and link with backend complete with key from env file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
 import './App.css';
+import axios from 'axios';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/home';
+
+const apiKey = process.env.API_KEY;
 
 function App() {
-  return <h1>hello</h1>;
+  // axios
+  //   .get('https://irmeohnkzcyspiqkwyjb.supabase.co/rest/v1/properties', {
+  //     headers: {
+  //       apikey: apiKey,
+  //     },
+  //   })
+  //   .then(({ data }) => {
+  //     console.log(data);
+  //   })
+  //   .catch((error) => {
+  //     console.log(error);
+  //   });
+
+  return (
+    <div>
+      <h1>AirBnb</h1>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
